063a0c4b1bd7391298ac9a3659177de81e7e2462
adityasurana3 <suranaaditya3@gmail.com>
2023-07-27 18:47:03 +0545

    insertion sort code

--- Insertion Sort/insertion_sort.cpp
+++ Insertion Sort/insertion_sort.cpp
@@ -1,5 +1,30 @@
 #include<iostream>
+using namespace std;
+
+void printArray(int arr[], int n){
+    for(int i=0;i<n;i++){
+        cout<<arr[i];
+    }
+}
 
 int main(){
+    int arr[5] = {4,1,6,7,2};
+    int n = sizeof(arr)/sizeof(int);
+    for(int i=1;i<n;i++){
+        int temp = arr[i];
+        int j = i-1;
+        for(;j>=0;j--){
+            if(arr[j]>temp){
+                arr[j+1] = arr[j];
+            }
+            else
+            {
+                break;
+            }
+            
+        }
+        arr[j+1] = temp;
+    }
+    printArray(arr, n);
     return 0;
 }
